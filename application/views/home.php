925daf3d33d500bba81780f7dc22ecaab7cdff6e
Francisco Navas <navasfran98@gmail.com>
2019-01-14 16:44:02 -0600

    template and layout

---
+++ application/views/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio</title>
+	<?php require 'layout/bootstrap.php' ?> <!--BOOSTRAP Y MATERIAL-->
+</head>
+<body>
+    <!--INICIO DE LA APLICACION-->
+
+    <!--FIN DE LA APLICACION-->
+	<?php require 'layout/footer.php' ?>
+</body>
+</html>
